d6887c3587aa3cccd52d3d6873765f3d70705ab1
Patrick Miller <pamiller.pdx@gmail.com>
2023-01-21 02:42:45 +0900

    Add pnpm and GH action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import rakkas from 'rakkasjs/vite-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { cloudflareWorkersCustomAdapter } from './build-tools/cloudflare-workers-adapter';
+// import { cloudflareWorkersCustomAdapter } from './build-tools/cloudflare-workers-adapter';
 
 export default defineConfig({
   plugins: [
